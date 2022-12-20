b8a971304ad4172a9bab2031003bfd1bc674f619
KevvKo <kklein915@gmail.com>
2022-12-20 14:36:05 +0100

    eslint fixes

--- src/packages/react-views-swiper-core/core/core.tsx
+++ src/packages/react-views-swiper-core/core/core.tsx
@@ -1,7 +1,7 @@
 import { MouseEvent, TouchEvent } from "react";
 
 export const getPositionX = (event: any) => {
-    return event.type.includes('mouse') ? event.pageX : event.touches[0].clientX
-}
+    return event.type.includes('mouse') ? event.pageX : event.touches[0].clientX;
+};
 
 export default getPositionX;
--- src/packages/react-views-swiper/components/View.tsx
+++ src/packages/react-views-swiper/components/View.tsx
@@ -1,6 +1,15 @@
-import React, { useLayoutEffect, ReactNode, useEffect ,useState, CSSProperties, createRef, MouseEvent, TouchEvent } from 'react';
+import React, { 
+    useLayoutEffect, 
+    ReactNode, 
+    useEffect,
+    useState, 
+    CSSProperties, 
+    createRef, 
+    MouseEvent, 
+    TouchEvent 
+} from 'react';
 import { getPositionX } from '../../react-views-swiper-core';
-import './View.css'
+import './View.css';
 
 interface ViewProps {
     children: ReactNode| ReactNode[];
@@ -21,77 +30,77 @@ const root: CSSProperties = {
     width: '100%',
     flexShrink: 0,
     transition: 'left 0.5s ease-out',
-}
+};
 
 const styles = {
     root,
-}
+};
 
 const View = ({children, hidden, index, viewCount, currentIndex, onChangeIndex, setTranslation, translation }: ViewProps) => {
     
-    const viewRef = createRef<HTMLDivElement>()
+    const viewRef = createRef<HTMLDivElement>();
     const [viewWidth, setViewWidth] = useState(0);
-    const [isDragging, setIsDragging] = useState(false)
-    const [startPosition, setStartPosition ] = useState(0)
+    const [isDragging, setIsDragging] = useState(false);
+    const [startPosition, setStartPosition ] = useState(0);
 
-    const handleTouchStart = (event: MouseEvent | TouchEvent, index: number) => {
-        setIsDragging(true)
+    const handleTouchStart = (event: MouseEvent | TouchEvent) => {
+        setIsDragging(true);
 
-        setStartPosition(getPositionX(event))
-        if(viewRef.current) viewRef.current?.classList.add('grabbing')         
-    }
+        setStartPosition(getPositionX(event));
+        if(viewRef.current) viewRef.current?.classList.add('grabbing');   
+    };
         
     const handleTouchMove = (event: MouseEvent | TouchEvent) => {
         if (isDragging) {
-            const currentPosition = getPositionX(event)
-            const currentTranslate = currentPosition - startPosition
-            setTranslation(currentTranslate)
+            const currentPosition = getPositionX(event);
+            const currentTranslate = currentPosition - startPosition;
+            setTranslation(currentTranslate);
         }
-    }
+    };
 
-    const handleTouchEnd = (event: MouseEvent | TouchEvent) => {
+    const handleTouchEnd = () => {
 
         if(isDragging){
-            setIsDragging(false)
+            setIsDragging(false);
 
             if(translation < -viewWidth/2){
                 if(currentIndex !== viewCount -1) {
-                    onChangeIndex( currentIndex + 1)
-                    setTranslation(0)
-                    return
+                    onChangeIndex( currentIndex + 1);
+                    setTranslation(0);
+                    return;
                 }
             }
     
             if(translation > viewWidth/2){
                 if(currentIndex !== 0) {
-                    onChangeIndex( currentIndex - 1)
-                    setTranslation(0)
-                    return
+                    onChangeIndex( currentIndex - 1);
+                    setTranslation(0);
+                    return;
                 }
             }
 
-            setTranslation(0)
+            setTranslation(0);
         }
 
-        if(viewRef.current) viewRef.current?.classList.remove('grabbing')
+        if(viewRef.current) viewRef.current?.classList.remove('grabbing');
 
-    }
+    };
 
     useEffect(() => {
         if(viewRef && viewWidth === 0) {
             if(viewRef.current) {
-                setViewWidth( viewRef.current!.getBoundingClientRect().width)
+                setViewWidth( viewRef.current.getBoundingClientRect().width);
             }
         }
-    }, [viewWidth, viewRef])
+    }, [viewWidth, viewRef]);
 
     useLayoutEffect(() => {
-        if(viewRef.current) viewRef.current!.style.left = `-${viewWidth*currentIndex}px`
-    }, [currentIndex])
+        if(viewRef.current) viewRef.current.style.left = `-${viewWidth*currentIndex}px`;
+    }, [currentIndex]);
 
     useLayoutEffect(() => {
-            viewRef.current!.style.transform = `translateX(${translation}px)`
-    }, [translation])
+        if(viewRef.current) viewRef.current.style.transform = `translateX(${translation}px)`;
+    }, [translation]);
 
     return (
         <>
@@ -100,10 +109,10 @@ const View = ({children, hidden, index, viewCount, currentIndex, onChangeIndex, 
                 ref={viewRef} 
                 style={styles.root} 
                 aria-hidden={hidden}
-                onTouchStart={(event) => handleTouchStart(event, index!)}
+                onTouchStart={(event) => handleTouchStart(event)}
                 onTouchEnd={handleTouchEnd}
                 onTouchMove={(event) => handleTouchMove(event)}
-                onMouseDown={(event) => handleTouchStart(event, index!)}
+                onMouseDown={(event) => handleTouchStart(event)}
                 onMouseUp={handleTouchEnd}
                 onMouseMove={(event) => handleTouchMove(event)}
                 onMouseLeave={handleTouchEnd}
@@ -111,7 +120,7 @@ const View = ({children, hidden, index, viewCount, currentIndex, onChangeIndex, 
                 {children}
             </div>
         </>
-    )
-}
+    );
+};
 
 export default View;
